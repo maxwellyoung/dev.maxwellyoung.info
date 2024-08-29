bec496c6b757ddc0df9b5b5adc77bce5ad5d6072
Maxwell Young <maxtheyoung@gmail.com>
2024-08-29 20:32:41 +1200

    landing page for studentview

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -46,7 +46,7 @@ const projects: Project[] = [
       "A companion app for students with budgeting, note-taking, and mental well-being tools.",
     longDescription:
       "StudentView is designed to be a focused version of Notion for students, encompassing budgeting, study reflections, and mental well-being tools. The app features a modular layout with both dark and light themes based on system preferences. It includes sections for tracking budgeting, study reflections, and a meditation section. Future plans involve integrating daily inspirational quotes and more user customization options.",
-    link: "https://dashboard.studentview.app/",
+    link: "https://www.studentview.app/",
   },
   {
     name: "ResumeForge",
