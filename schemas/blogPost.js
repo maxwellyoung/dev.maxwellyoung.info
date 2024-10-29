09184787e67a62f323357227f2b04be9d17092b3
Maxwell Young <maxtheyoung@gmail.com>
2024-10-29 15:22:10 +1300

    blog and chlita

---
+++ schemas/blogPost.js
@@ -0,0 +1,60 @@
+const blogPostSchema = {
+  name: "blogPost",
+  title: "Blog Post",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+    },
+    {
+      name: "publishedAt",
+      title: "Published at",
+      type: "datetime",
+    },
+    {
+      name: "excerpt",
+      title: "Excerpt",
+      type: "text",
+    },
+    {
+      name: "content",
+      title: "Content",
+      type: "array",
+      of: [{ type: "block" }],
+    },
+    {
+      name: "tags",
+      title: "Tags",
+      type: "array",
+      of: [{ type: "string" }],
+    },
+    {
+      name: "mainImage",
+      title: "Main image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          title: "Alternative text",
+          type: "string",
+        },
+      ],
+    },
+  ],
+};
+
+export default blogPostSchema;
