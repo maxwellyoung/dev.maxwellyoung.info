c002c3b83a1817f9de2b1701b1f0010e45782b9d
Maxwell Young <maxtheyoung@gmail.com>
2026-07-25 19:49:06 +1200

    canon: refresh currently-into feed

--- src/lib/canonFeed.ts
+++ src/lib/canonFeed.ts
@@ -68,8 +68,8 @@ export const canonFeed: CanonFeed = {
     },
     {
       "verb": "in rotation",
-      "title": "Green",
-      "creator": "Lil Damn",
+      "title": "1kiss",
+      "creator": "Maxwell Young",
       "art": {
         "src": "/canon/rotation.jpg",
         "w": 360,
