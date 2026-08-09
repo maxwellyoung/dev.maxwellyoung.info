4ac829369925e2141417a9831c974e13f5c96fa1
Maxwell Young <maxtheyoung@gmail.com>
2026-08-09 14:39:37 +1200

    canon: refresh currently-into feed

--- src/lib/canonFeed.ts
+++ src/lib/canonFeed.ts
@@ -48,8 +48,8 @@ export const canonFeed: CanonFeed = {
     },
     {
       "verb": "playing",
-      "title": "TowerFall Ascension",
-      "creator": "Maddy Makes Games Inc., Extremely OK Games, Ltd.",
+      "title": "Counter-Strike 2",
+      "creator": "Valve",
       "art": {
         "src": "/canon/playing.jpg",
         "w": 240,
