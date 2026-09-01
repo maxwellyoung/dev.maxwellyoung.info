eb32a941c4f1b795681aafe0eeb85bb7756b05ad
Maxwell Young <maxtheyoung@gmail.com>
2026-09-02 08:14:31 +1200

    canon: refresh currently-into feed

--- src/lib/canonFeed.ts
+++ src/lib/canonFeed.ts
@@ -31,7 +31,7 @@ export interface CanonFeed {
 
 export const canonFeed: CanonFeed = {
   "generatedAt": "2026-09-01",
-  "sourceSyncedAt": "2026-09-01T19:43:31.362Z",
+  "sourceSyncedAt": "2026-09-01T20:14:21.508Z",
   "totalWorks": 4278,
   "regionCount": 22,
   "regions": [
